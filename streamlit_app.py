179f6d5f4fcecd1d9365a5f6c15ff515ce98cb3d
bhavya-appdev4ever <138752042+bhavya-appdev4ever@users.noreply.github.com>
2023-07-06 11:11:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Dinner Menu')
 streamlit.text('Oranges')
 streamlit.text('Apple')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
